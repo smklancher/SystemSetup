5bf0079f6df4a779841c67254a5737cddafcfca3
Stephen Klancher <smknight@gmail.com>
2019-08-06 15:50:28 -0700

    q

--- scripts/Git.ps1
+++ scripts/Git.ps1
@@ -1,4 +1,4 @@
-
-choco install -y git --package-parameters="`"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration`""
+cinst git -y --package-parameters="/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"
+#choco install -y git --package-parameters="`"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration`""
 choco install -y tortoisegit
 refreshenv
